d293d96f6d071d4e21b8f8d1957faa9f16cbd122
Jonnatas <jonatas_lenon@hotmail.com>
2017-10-31 23:04:04 -0200

    add login screnn

--- app/src/main/java/com/example/jonnatas/itsokay/LoginActivity.java
+++ app/src/main/java/com/example/jonnatas/itsokay/LoginActivity.java
@@ -1,13 +1,34 @@
 package com.example.jonnatas.itsokay;
 
+import android.app.ProgressDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private FirebaseAuth firebaseAuth;
+    EditText editTextLogin;
+    EditText editTextPassword;
+    Button buttonLogin;
+    Button buttonRegister;
+    ProgressDialog progressLogin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
