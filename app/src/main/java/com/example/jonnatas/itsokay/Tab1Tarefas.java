6dd0769866f953e2543487584759908d24b9ec99
Jonnatas <jonatas_lenon@hotmail.com>
2017-11-19 16:07:28 -0200

    add array adapter de tarefas

--- app/src/main/java/com/example/jonnatas/itsokay/Tab1Tarefas.java
+++ app/src/main/java/com/example/jonnatas/itsokay/Tab1Tarefas.java
@@ -5,16 +5,45 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import com.example.jonnatas.itsokay.config.ConfiguracaoFirebase;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.ArrayList;
 
 /**
  * Created by jonnatas on 17/11/17.
  */
 
 public class Tab1Tarefas extends Fragment {
+
+    DatabaseReference reference = ConfiguracaoFirebase.getFirebase();
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private ArrayList<String> tarefas;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         View rootView = inflater.inflate(R.layout.tabs1tarefas, container, false);
+
+        tarefas = new ArrayList<>();
+        tarefas.add("Seja feliz");
+        tarefas.add("Seja triste");
+        tarefas.add("Seja qualquer coisa");
+
+        listView = (ListView) rootView.findViewById(R.id.lv_tarefas);
+        adapter = new ArrayAdapter(
+                getActivity(),
+                android.R.layout.simple_list_item_checked,
+                tarefas
+        );
+
+        listView.setAdapter(adapter);
 
         return rootView;
     }
